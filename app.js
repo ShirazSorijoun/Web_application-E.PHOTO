ba8c57c7fb0314bdde955976db5480af15f76c24
Daniel Hay <daniel.hay@cybereason.com>
2023-07-30 16:12:43 +0300

    server

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 //js
 const express = require('express');
+var connect = require("connect");
 const bodyParser = require('body-parser');
 const cors = require('cors')
 const mongoose = require('mongoose');
 const Orders = require('./routes/R_order');
 const newLocal = require('custom-env')  
+
 newLocal.env(process.env.NODE_ENV,'./config');
+
 
 // Mongo DB conncetion
 const database = process.env.CONNECTION_STRING || "mongodb://127.0.0.1:27017/proddb";
@@ -15,13 +18,19 @@ mongoose.connect(database,
     .catch(err => console.log(err));
 
 
-const app = express();
+ var app = express().use(express.static(__dirname + '/views/views'));
+ 
+ app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});  
+
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.json());
 
 
 //Routes
+
 app.use('/cart', Orders);
 const PORT = process.env.PORT ||  4111;
 app.listen(PORT, console.log("Server do start for port: " + PORT))
