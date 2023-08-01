56e0616c574f218aa83006913955e591cd9fdeea
Shiraz <shirazsorijoun@gmail.com>
2023-08-01 13:30:00 +0300

    userSchema - update

--- models/M_User.js
+++ models/M_User.js
@@ -1,75 +1,38 @@
-/*
-const mongoose = require ('mongoose');
-
-mongoose.connect('mongodb://<db_username>:<db_password>@<db_host>/<db_name>', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;s
-db.on('error', console.error.bind(console, 'Connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB!');
-});
-
+const mongoose = require ("mongoose");
 
 const userSchema = new mongoose.Schema({
     email:{
         type:String,
-        required:true,
+        required: [true, "Email is required."],
         unique: true
     },
     password:{
         type:String,
-        required:true
+        required: [true, "Password is required."]
+    },
+    firstName: {
+      type: String,
+      required: [true, "First name is required."]
+    },
+    lastName: {
+      type: String,
+      required: [true, "Last name is required."]
     },
     type:{
         type:String,
-        required:true
+        required: [true, "Type is required."]
     },
     cart:[{
         type:mongoose.Schema.Types.ObjectId,
-        ref: 'M_photo'
+        ref: 'ShoppingBasketSchema'
     }],
-    boughtPhotos:[{
+    boughtProduct:[{
         type:mongoose.Schema.Types.ObjectId,
-        ref: 'M_photo'
-    }],
-    uploadedPhotos:[{
-        type:mongoose.Schema.Types.ObjectId,
-        ref: 'M_photo'
+        ref: 'productSchema'
     }]
 });
 
 // Create the User model
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 // Export the User model
-module.exports = User;
-*/
-
-
-const mongoose = require("mongoose");
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
- location: {     
-   type: String,    
-   default: "Rishon Lezion",
-   },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-});
-const User = mongoose.model("User", UserSchema);
 module.exports = User;
