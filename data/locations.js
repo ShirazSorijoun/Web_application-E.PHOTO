5482d6cf93fe3a93c87a6810eb1881eb5a12fee4
Danielle Rotem <rotemdanielle@gmail.com>
2023-08-02 00:01:36 +0300

    made modifications to the app.js, contact.html and all location related files. there is a load of locations into the DB, safe usage of map key, inti the map using info pulled from the DB,changed form submision function. need to fix - the map is loading but fails after a that.

---
+++ data/locations.js
@@ -0,0 +1,8 @@
+const storeLocations = [
+    { lat: 31.96991090840196, lng: 34.77201473451009, name: 'Main office' },
+    { lat: 31.97007473621481, lng: 34.77105986810615, name: 'Consultant office' },
+  ];
+
+  module.exports = storeLocations;
+
+
--- models/M_location.js
+++ models/M_location.js
@@ -1,19 +1,5 @@
 const mongoose = require ('mongoose');
 
-/*
-mongoose.connect('mongodb://<db_username>:<db_password>@<db_host>/<db_name>', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB!');
-});
-
-*/
-
 const storeLocationSchema = new mongoose.Schema({
   name: {
     type: String,
