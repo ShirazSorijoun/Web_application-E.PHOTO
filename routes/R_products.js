db9612eace7d92c1fc333d6e67bbd8fa93bb4dfe
Daniel Hay <daniel.hay@cybereason.com>
2023-08-02 09:58:18 +0300

    server

--- routes/R_products.js
+++ routes/R_products.js
@@ -2,6 +2,18 @@ const  express = require('express');
 const  router = express.Router();
 const  Products  = require('../controllers/C_products'); // Correct the import statement
 
+
+
+
+router.get('/api/products', async (req, res, next) => {
+    try {
+      const products = await Product.find({});
+      res.json(products);
+    } catch (error) {
+      next(error);
+    }
+  });
+
 router.route('/')
     .get(Products.getProducts)
     
